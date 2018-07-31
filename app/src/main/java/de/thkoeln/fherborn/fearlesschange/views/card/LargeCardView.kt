33c28124d8ace529a1a34fe2af5832a77cfab31d
fherborn <florianherborn2293@t-online.de>
2018-07-31 07:47:40 +0200

    Added Listeners to Cardview

--- app/src/main/java/de/thkoeln/fherborn/fearlesschange/views/card/LargeCardView.kt
+++ app/src/main/java/de/thkoeln/fherborn/fearlesschange/views/card/LargeCardView.kt
@@ -1,7 +1,6 @@
 package de.thkoeln.fherborn.fearlesschange.views.card
 
 import android.content.Context
-import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import de.thkoeln.fherborn.fearlesschange.db.Card
 import android.view.LayoutInflater
@@ -14,12 +13,27 @@ import kotlinx.android.synthetic.main.layout_card_large.view.*
  */
 class LargeCardView : CardView {
 
+    var onCardClickedListener: ((LargeCardView, Card?) -> Unit)? = null
+    var onFavoriteClickedListener: ((LargeCardView, Card?) -> Unit)? = null
+    var onNotesClickedListener: ((LargeCardView, Card?) -> Unit)? = null
+
     constructor(context: Context): super(context)
     constructor(context: Context, attributeSet: AttributeSet?): super(context, attributeSet)
     constructor(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int): super(context, attributeSet, defStyleAttr)
 
     override fun onCreateView(inflater: LayoutInflater, context: Context, attributeSet: AttributeSet?) {
         inflater.inflate(R.layout.layout_card_large, this, true)
+        onSetListeners()
+    }
+
+    private fun onSetListeners() {
+        setOnClickListener { view ->
+            when(view) {
+                card_fav_btn -> onFavoriteClickedListener?.invoke(this, card)
+                card_note_btn -> onNotesClickedListener?.invoke(this, card)
+                else -> onCardClickedListener?.invoke(this, card)
+            }
+        }
     }
 
     override fun onCardChanged(card: Card?) {
