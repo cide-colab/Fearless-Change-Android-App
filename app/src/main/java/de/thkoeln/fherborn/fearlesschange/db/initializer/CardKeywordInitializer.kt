37817f62db1f6075d6a6d91efb5eae7b54d2ba82
Florian Herborn <florian.herborn@th-koeln.de>
2018-08-06 07:08:44 +0200

    Added more Initializer

--- app/src/main/java/de/thkoeln/fherborn/fearlesschange/db/initializer/CardKeywordInitializer.kt
+++ app/src/main/java/de/thkoeln/fherborn/fearlesschange/db/initializer/CardKeywordInitializer.kt
@@ -4,12 +4,16 @@ import android.content.Context
 import de.thkoeln.fherborn.fearlesschange.db.CardDatabase
 import de.thkoeln.fherborn.fearlesschange.db.CardKeyword
 
-class CardKeywordInitializer(val context: Context) {
-    fun onInsert() {
-        CardDatabase.getInstance(context).cardKeywordDao().insertCardKeywords(listOf(
-                CardKeyword(0, "Keyword 1"),
-                CardKeyword(0, "Keyword 2"),
-                CardKeyword(1, "Keyword 2")
-        ))
-    }
+class CardKeywordInitializer(val context: Context): DataInitializer<CardKeyword>("card_keyword") {
+
+    override fun getItemValues(item: CardKeyword) = listOf(
+            item.card_id,
+            item.keyword
+    )
+
+    override fun getItems() = listOf(
+            CardKeyword(0, "Keyword 1"),
+            CardKeyword(0, "Keyword 2"),
+            CardKeyword(1, "Keyword 2")
+    )
 }
--- app/src/main/java/de/thkoeln/fherborn/fearlesschange/db/initializer/KeywordInitializer.kt
+++ app/src/main/java/de/thkoeln/fherborn/fearlesschange/db/initializer/KeywordInitializer.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import de.thkoeln.fherborn.fearlesschange.db.CardDatabase
 import de.thkoeln.fherborn.fearlesschange.db.Keyword
 
-class KeywordInitializer(val context: Context) {
-    fun onInsert() {
-        CardDatabase.getInstance(context).keywordDao().insertKeywords(listOf(
-                Keyword("Keyword 1"),
-                Keyword("Keyword 2"),
-                Keyword("Keyword 3")
-        ))
-    }
+class KeywordInitializer(val context: Context): DataInitializer<Keyword>("keyword") {
+    override fun getItemValues(item: Keyword) = listOf(
+            item.keyword
+    )
+
+    override fun getItems() = listOf(
+            Keyword("Keyword 1"),
+            Keyword("Keyword 2"),
+            Keyword("Keyword 3")
+    )
 }
