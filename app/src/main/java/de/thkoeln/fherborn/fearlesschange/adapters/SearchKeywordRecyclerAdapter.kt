a969b60875729e66dd5f23339eb072b01147906c
Jannik <jannik-bl@web.de>
2018-08-20 13:35:16 +0200

    Zwischenstand Search

--- app/src/main/java/de/thkoeln/fherborn/fearlesschange/adapters/SearchKeywordRecyclerAdapter.kt
+++ app/src/main/java/de/thkoeln/fherborn/fearlesschange/adapters/SearchKeywordRecyclerAdapter.kt
@@ -1,16 +1,18 @@
 package de.thkoeln.fherborn.fearlesschange.adapters
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import de.thkoeln.fherborn.fearlesschange.R
 import de.thkoeln.fherborn.fearlesschange.persistance.models.Keyword
 
-class SearchKeywordRecyclerAdapter(var keywords: List<Keyword> = listOf()) :
+class SearchKeywordRecyclerAdapter(var keywords: MutableList<Keyword> = mutableListOf()) :
         RecyclerView.Adapter<SearchKeywordRecyclerAdapter.KeywordListViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KeywordListViewHolder {
-        return KeywordListViewHolder(TextView(parent.context))
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_keyword_list_item, parent, false)
+        return KeywordListViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -18,10 +20,13 @@ class SearchKeywordRecyclerAdapter(var keywords: List<Keyword> = listOf()) :
     }
 
     override fun onBindViewHolder(holder: KeywordListViewHolder, position: Int) {
-        holder.textView.text = keywords[position].keyword
+        holder.bind(keywords[position].keyword)
     }
 
     inner class KeywordListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var textView: TextView = itemView.findViewById(R.id.selected_keywords)
+        private val textView = itemView.findViewById<TextView>(R.id.keyword_list_item_textview)
+        fun bind (keyword: String) {
+            textView.text = keyword
+        }
     }
 }
--- app/src/main/java/de/thkoeln/fherborn/fearlesschange/ui/activities/SearchActivity.kt
+++ app/src/main/java/de/thkoeln/fherborn/fearlesschange/ui/activities/SearchActivity.kt
@@ -4,8 +4,11 @@ import android.arch.lifecycle.Observer
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
+import android.support.v7.widget.helper.ItemTouchHelper
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
@@ -19,33 +22,37 @@ import kotlinx.android.synthetic.main.layout_default_app_bar.*
 
 class SearchActivity : AppCompatActivity() {
     private lateinit var keywordRepository: KeywordRepository
-    private lateinit var textView: AutoCompleteTextView
-    private lateinit var selectedKeywords: MutableList<Keyword>
-    private lateinit var selectedKeywordsView: RecyclerView
     private lateinit var selectedKeywordsAdapter: SearchKeywordRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         setupActionBar()
-        selectedKeywords = mutableListOf()
 
         keywordRepository = KeywordRepository(application)
 
-        this.textView = findViewById(R.id.search_keyword)
-
         keywordRepository.getAllKeywords().observe(this, Observer { keywords ->
             val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, keywords)
-            textView.setAdapter(adapter)
+            search_keyword.setAdapter(adapter)
         })
 
-        this.selectedKeywordsAdapter = SearchKeywordRecyclerAdapter(selectedKeywords)
-        this.selectedKeywordsView = findViewById(R.id.selected_keywords)
-        this.selectedKeywordsView.adapter = selectedKeywordsAdapter
+        this.selectedKeywordsAdapter = SearchKeywordRecyclerAdapter()
+        selected_keywords.layoutManager = LinearLayoutManager(this)
+        selected_keywords.adapter = selectedKeywordsAdapter
 
-        add_keyword.setOnClickListener{
+        add_keyword.setOnClickListener {
             addKeyword()
         }
+
+        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, target: RecyclerView.ViewHolder?) = false
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, swipeDir: Int) {
+                when (swipeDir) {
+                    ItemTouchHelper.LEFT -> removeKeyword(selectedKeywordsAdapter.keywords[viewHolder.adapterPosition])
+                }
+            }
+        })
+        itemTouchHelper.attachToRecyclerView(selected_keywords)
     }
 
     /**
@@ -57,20 +64,25 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private fun addKeyword() {
-        val keywordInput: String = this.textView.text.toString()
+        val keywordInput: String = search_keyword.text.toString()
         keywordRepository.getKeywordByKeyword(keywordInput).observe(this, Observer { selectedKeyword ->
             selectedKeyword?.let {
-                val elementExists = this.selectedKeywords.find { keyword ->  keyword.keyword.equals(keywordInput)}
-                elementExists?.let {
-                    this.selectedKeywords.add(selectedKeyword)
-                    this.selectedKeywordsAdapter.notifyDataSetChanged()
-                } ?: run {
+                val elementExists = selectedKeywordsAdapter.keywords.contains(selectedKeyword)
+                if (!elementExists) {
+                    selectedKeywordsAdapter.keywords.add(selectedKeyword)
+                    selectedKeywordsAdapter.notifyDataSetChanged()
+                } else {
                     // TODO: Fehler, dass Element bereits vorhanden ist
                 }
             } ?: run {
                 // TODO: Fehlermeldung ausgeben, dass das Keyword nicht gefunden wurde
             }
         })
+    }
+
+    private fun removeKeyword(keywordToRemove: Keyword) {
+        selectedKeywordsAdapter.keywords.remove(keywordToRemove)
+        selectedKeywordsAdapter.notifyDataSetChanged()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
