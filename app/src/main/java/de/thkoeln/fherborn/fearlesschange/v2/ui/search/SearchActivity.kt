1ff56925612e0b03121ec6b6dfa20269e4afca18
fherborn <florianherborn2293@t-online.de>
2018-08-27 07:06:54 +0200

    Cleaned menus

--- app/src/main/java/de/thkoeln/fherborn/fearlesschange/v2/ui/search/SearchActivity.kt
+++ app/src/main/java/de/thkoeln/fherborn/fearlesschange/v2/ui/search/SearchActivity.kt
@@ -1,15 +1,11 @@
 package de.thkoeln.fherborn.fearlesschange.v2.ui.search
 
 import android.arch.lifecycle.ViewModelProviders
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
-import android.view.Menu
-import android.view.MenuItem
 import de.thkoeln.fherborn.fearlesschange.R
 import de.thkoeln.fherborn.fearlesschange.v2.data.viewmodel.SettingsViewModel
 import de.thkoeln.fherborn.fearlesschange.v2.ui.AppActivity
-import kotlinx.android.synthetic.main.activity_search.*
 import kotlinx.android.synthetic.main.action_bar.*
 
 class SearchActivity : AppActivity() {
@@ -19,18 +15,5 @@ class SearchActivity : AppActivity() {
         setContentView(R.layout.activity_search)
         setSupportActionBar(action_bar as Toolbar)
         val viewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.action_bar_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.action_search -> {
-            startActivity(Intent(this, SearchActivity::class.java))
-            true
-        }
-        else -> super.onOptionsItemSelected(item)
     }
 }
