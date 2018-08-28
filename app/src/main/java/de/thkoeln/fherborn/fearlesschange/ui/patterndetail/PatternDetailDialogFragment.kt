631ff5578353f7f40b4d325232fbc5407303242b
tyll.hovestadt <tyll.hovestadt@meinestadt.de>
2018-08-28 15:13:20 +0200

    added note count to card detail.

--- app/src/main/java/de/thkoeln/fherborn/fearlesschange/ui/patterndetail/PatternDetailDialogFragment.kt
+++ app/src/main/java/de/thkoeln/fherborn/fearlesschange/ui/patterndetail/PatternDetailDialogFragment.kt
@@ -4,14 +4,19 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import de.thkoeln.fherborn.fearlesschange.R
 import de.thkoeln.fherborn.fearlesschange.data.persistance.pattern.Pattern
+import de.thkoeln.fherborn.fearlesschange.data.persistance.pattern.PatternInfo
 import de.thkoeln.fherborn.fearlesschange.data.viewmodel.PatternViewModel
 import de.thkoeln.fherborn.fearlesschange.ui.adapter.PatternCardBackAdapter
 import de.thkoeln.fherborn.fearlesschange.ui.adapter.PatternCardFrontAdapter
+import de.thkoeln.fherborn.fearlesschange.ui.customs.card.PatternCardDelegation
 import de.thkoeln.fherborn.fearlesschange.ui.notes.PatternNotesFragment
 import kotlinx.android.synthetic.main.pattern_detail_dialog.*
 
@@ -61,10 +66,18 @@ class PatternDetailDialogFragment : DialogFragment() {
         viewModel.getPattern(cardId).observe(this, Observer { onCardUpdate(it) })
     }
 
-    private fun onCardUpdate(card: Pattern?) {
-
-        cardFrontAdapter.change(card)
-        cardBackAdapter.change(card)
+    private fun onCardUpdate(card: PatternInfo?) {
+        card?.noteCount.let {
+            val visibility = when {
+                it != null && it > 0 -> VISIBLE
+                else -> INVISIBLE
+            }
+            card_detail_notes_count.visibility = visibility
+            card_detail_notes_icon.visibility = visibility
+            card_detail_notes_count.text = it?.toString()
+        }
+        cardFrontAdapter.change(card?.pattern)
+        cardBackAdapter.change(card?.pattern)
     }
 
     companion object {
