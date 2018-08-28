b783e5064202ed31fe289d9ac49b7056c4eaae9b
tyll.hovestadt <tyll.hovestadt@meinestadt.de>
2018-08-28 10:33:05 +0200

    Testcode for tutorials.

--- app/src/main/java/de/thkoeln/fherborn/fearlesschange/utils/DashboardShowcaseViews.kt
+++ app/src/main/java/de/thkoeln/fherborn/fearlesschange/utils/DashboardShowcaseViews.kt
@@ -12,60 +12,41 @@ class DashboardShowcaseViews(private val activity: DashboardActivity) {
         overviewShowcase()
     }
 
-    private fun getPreparedShowcaseBuilder(viewTargetId: Int, titleId: Int, next: SimpleShowcaseEventListener) = ShowcaseView.Builder(activity)
+    private fun getPreparedShowcaseBuilder(viewTargetId: Int, titleId: Int, textId: Int, next: () -> Unit = {}) = ShowcaseView.Builder(activity)
             .setTarget(ViewTarget(viewTargetId, activity))
             .setContentTitle(titleId)
+            .setContentText(textId)
             .setStyle(R.style.MyShowCaseView)
             .hideOnTouchOutside()
-            .setShowcaseEventListener(next)
+            .setShowcaseEventListener(object : SimpleShowcaseEventListener() {
+                override fun onShowcaseViewDidHide(showcaseView: ShowcaseView?) {
+                    next.invoke()
+                }
+            })
 
     private fun overviewShowcase() {
-        getPreparedShowcaseBuilder(R.id.fab_overview, R.string.overview_title, object : SimpleShowcaseEventListener() {
-                    override fun onShowcaseViewDidHide(showcaseView: ShowcaseView?) {
-                        favoriteShowcase()
-                    }
-                })
+        getPreparedShowcaseBuilder(R.id.fab_overview, R.string.overview_showcase_title, R.string.overview_showcase_text) { favoriteShowcase() }
                 .singleShot(1)
                 .build()
     }
 
     private fun favoriteShowcase() {
-        val viewTarget = ViewTarget(R.id.fab_favorites, activity)
-        ShowcaseView.Builder(activity)
-                .setTarget(viewTarget)
-                .setContentTitle("Here you can see your favorite cards!")
-                .setStyle(R.style.MyShowCaseView)
-                .hideOnTouchOutside()
-                .setShowcaseEventListener(object : SimpleShowcaseEventListener() {
-                    override fun onShowcaseViewDidHide(showcaseView: ShowcaseView?) {
-                        moreShowcase()
-                    }
-                })
+        getPreparedShowcaseBuilder(R.id.fab_favorites, R.string.favorites_showcase_title, R.string.favorites_showcase_text) { settingsShowcase() }
                 .build()
     }
 
-    private fun moreShowcase() {
-        val viewTarget = ViewTarget(R.id.fab_more, activity)
-        ShowcaseView.Builder(activity)
-                .setTarget(viewTarget)
-                .setContentTitle("Here you can see more cool things!")
-                .setStyle(R.style.MyShowCaseView)
-                .hideOnTouchOutside()
-                .setShowcaseEventListener(object : SimpleShowcaseEventListener() {
-                    override fun onShowcaseViewDidHide(showcaseView: ShowcaseView?) {
-                        cardOfTheDayShowcase()
-                    }
-                })
+    private fun settingsShowcase() {
+        getPreparedShowcaseBuilder(R.id.fab_more, R.string.settings_showcase_title, R.string.settings_showcase_text) { cardOfTheDayShowcase() }
                 .build()
     }
 
     private fun cardOfTheDayShowcase() {
-        val viewTarget = ViewTarget(R.id.card_of_the_day, activity)
-        ShowcaseView.Builder(activity)
-                .setTarget(viewTarget)
-                .setContentTitle("This is the card of the day!")
-                .hideOnTouchOutside()
-                .setStyle(R.style.MyShowCaseView)
+        getPreparedShowcaseBuilder(R.id.card_of_the_day, R.string.card_of_the_day_showcase_title, R.string.card_of_the_day_showcase_text) {randomCardsShowcase()}
+                .build()
+    }
+
+    private fun randomCardsShowcase() {
+        getPreparedShowcaseBuilder(R.id.random_cards_2, R.string.random_card_showcase_title, R.string.random_card_showcase_text)
                 .build()
     }
 }
