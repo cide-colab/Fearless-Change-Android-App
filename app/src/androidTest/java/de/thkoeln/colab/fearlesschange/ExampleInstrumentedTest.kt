e2da9de90fead934e6902a4e0d807b2b42187db3
Florian Herborn <florian.herborn@gmail.com>
2019-06-25 14:33:23 +0200

    Added images for playstore

--- app/src/androidTest/java/de/thkoeln/colab/fearlesschange/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/de/thkoeln/colab/fearlesschange/ExampleInstrumentedTest.kt
@@ -1,24 +1,19 @@
 package de.thkoeln.colab.fearlesschange
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented text, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under text.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("de.thkoeln.fherborn.fearlesschange", appContext.packageName)
+        val expected = "de.thkoeln.fherborn.fearlesschange"
+        val actual = InstrumentationRegistry.getInstrumentation().targetContext.packageName
+        assertEquals(expected, actual)
     }
 }
