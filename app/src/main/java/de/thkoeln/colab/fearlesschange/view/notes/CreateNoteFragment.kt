21d99cbc48eebb15ffad45cdddc38b2808c09d93
fherborn <florianherborn2293@t-online.de>
2019-08-23 15:08:05 +0200

    Added RichtTextView

--- app/src/main/java/de/thkoeln/colab/fearlesschange/view/notes/CreateNoteFragment.kt
+++ app/src/main/java/de/thkoeln/colab/fearlesschange/view/notes/CreateNoteFragment.kt
@@ -9,9 +9,11 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
 import de.thkoeln.colab.fearlesschange.R
 import kotlinx.android.synthetic.main.create_note_fragment.*
+import org.wordpress.aztec.Aztec
+import org.wordpress.aztec.ITextFormat
+import org.wordpress.aztec.toolbar.IAztecToolbarClickListener
 
-class CreateNoteFragment : Fragment() {
-
+class CreateNoteFragment : Fragment(), IAztecToolbarClickListener {
     private val args: CreateNoteFragmentArgs by navArgs()
 
     companion object {
@@ -34,9 +36,15 @@ class CreateNoteFragment : Fragment() {
         viewModel = createViewModel()
 
         val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+        imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
 
-        create_note_title.requestFocus()
+//        create_note_title.requestFocus()
+        create_note_note.requestFocus()
+
+        Aztec.with(create_note_note, create_note_toolbar, this)
+//                .setImageGetter(GlideImageLoader(context))
+//                .setVideoThumbnailGetter(GlideVideoThumbnailLoader(context))
+
 
     }
 
@@ -50,12 +58,38 @@ class CreateNoteFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_create_note -> {
-                viewModel.onCreateNoteClicked(create_note_title.text.toString(), create_note_note.text.toString())
+//                viewModel.onCreateNoteClicked(create_note_title.text.toString(), create_note_note.text.toString())
+
+                val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view?.windowToken, 0)
+
                 requireActivity().onBackPressed()
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onToolbarExpandButtonClicked() {
+    }
+
+    override fun onToolbarFormatButtonClicked(format: ITextFormat, isKeyboardShortcut: Boolean) {
+    }
+
+    override fun onToolbarHeadingButtonClicked() {
+    }
+
+    override fun onToolbarHtmlButtonClicked() {
+    }
+
+    override fun onToolbarListButtonClicked() {
+    }
+
+    override fun onToolbarMediaButtonClicked(): Boolean {
+        return false
+    }
+
+    override fun onToolbarCollapseButtonClicked() {
     }
 
 
