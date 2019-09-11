3902cae062ce07fdc68e8ecd5f12fbf6797e8323
fherborn <florianherborn2293@t-online.de>
2019-09-11 13:46:48 +0200

    Added option to hide bottom nav with bundle param. Hide nav on create note.

--- app/src/main/java/de/thkoeln/colab/fearlesschange/view/MainActivity.kt
+++ app/src/main/java/de/thkoeln/colab/fearlesschange/view/MainActivity.kt
@@ -1,7 +1,11 @@
 package de.thkoeln.colab.fearlesschange.view
 
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import de.thkoeln.colab.fearlesschange.R
@@ -16,10 +20,16 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(activity_toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         setupNav()
+
+
     }
 
     private fun setupNav() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
-        navHostFragment?.let { NavigationUI.setupWithNavController(bottom_nav, it.navController) }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(bottom_nav, navHostFragment.navController)
+        navHostFragment.navController.addOnDestinationChangedListener { _: NavController, _: NavDestination, bundle: Bundle? ->
+            val hideNav = bundle?.getBoolean("hide_nav", false) ?: false
+            bottom_nav.visibility = if (hideNav) GONE else VISIBLE
+        }
     }
 }
