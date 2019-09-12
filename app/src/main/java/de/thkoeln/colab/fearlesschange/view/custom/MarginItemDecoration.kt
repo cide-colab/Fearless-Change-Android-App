393dc71e1233f4fe4eee096c3cc3c5e3936e735d
fherborn <florianherborn2293@t-online.de>
2019-09-12 16:59:20 +0200

    Improved LayoutDecorator to add spacing Fixes #53

--- app/src/main/java/de/thkoeln/colab/fearlesschange/view/custom/MarginItemDecoration.kt
+++ app/src/main/java/de/thkoeln/colab/fearlesschange/view/custom/MarginItemDecoration.kt
@@ -2,17 +2,33 @@ package de.thkoeln.colab.fearlesschange.view.custom
 
 import android.graphics.Rect
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MarginItemDecoration(private val spaceHeight: Int) : RecyclerView.ItemDecoration() {
+class MarginItemDecoration(private val spacing: Int, private val includeEdge: Boolean = true) : RecyclerView.ItemDecoration() {
+
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        with(outRect) {
-            if (parent.getChildAdapterPosition(view) == 0) {
-                top = spaceHeight
+        val spanCount = when (val layoutManager = parent.layoutManager) {
+            is GridLayoutManager -> layoutManager.spanCount
+            else -> 1
+        }
+        val position = parent.getChildAdapterPosition(view)
+        val column = position % spanCount
+
+        if (includeEdge) {
+            outRect.left = spacing - column * spacing / spanCount
+            outRect.right = (column + 1) * spacing / spanCount
+
+            if (position < spanCount) {
+                outRect.top = spacing
             }
-            left = spaceHeight
-            right = spaceHeight
-            bottom = spaceHeight
+            outRect.bottom = spacing
+        } else {
+            outRect.left = column * spacing / spanCount
+            outRect.right = spacing - (column + 1) * spacing / spanCount
+            if (position >= spanCount) {
+                outRect.top = spacing
+            }
         }
     }
 }
